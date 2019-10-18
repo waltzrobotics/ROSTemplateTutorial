bd04cb4ef79a1a90eb3dd071fd8f07d6b2e7b7e3
waltzrobotics <johnwaltz@gmail.com>
2019-10-18 12:05:17 +0000

    delete clion cmake-build-debug

--- src/ros_outline/examples/7_packages/use_another_package/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wwaltz/Desktop/robot_notes/ros/src/ros_outline/examples/7_packages/use_another_package
-# Build directory: /home/wwaltz/Desktop/robot_notes/ros/src/ros_outline/examples/7_packages/use_another_package/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- src/ros_outline/examples/7_packages/use_another_package/cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "use_another_package"
-PROJECT_SPACE_DIR = "/home/wwaltz/Desktop/robot_notes/ros/src/ros_outline/examples/7_packages/use_another_package/cmake-build-debug/devel"
-PROJECT_VERSION = "0.0.0"
